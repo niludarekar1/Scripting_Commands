2d5c2d54855cc4ba003cad06d86e1995010b6913
root <root@ip-172-31-38-170.ap-south-1.compute.internal>
2026-04-09 05:16:42 +0000

    Initial Commit

---
+++ Script3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is cration and calling of shell variable --defining varibles
+class=Young-Minds
+Batch=32
+PROFESSION=AWS/DevSecOps/Azure DevOps
+echo "class Name is $class, Batch Number $Batch, We are learing $PROFESSION"
