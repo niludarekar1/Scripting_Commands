8631c10828143c94a155558eab76aecc760286f8
root <root@ip-172-31-38-170.ap-south-1.compute.internal>
2026-04-12 05:39:05 +0000

    Initial Commit

---
+++ Script-5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# test of few of the fixed variable
+echo "script name : $0"
+echo "1st cmdls: $1"
+echo "2nd cmds : $2"
+echo "3rd cmdls : $3"
+echo " no of commmands: $#"
--- Script3.sh
+++ Script3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is cration and calling of shell variable --defining varibles
-class=Young-Minds
+class="Young-Minds"
 Batch=32
-PROFESSION=AWS/DevSecOps/Azure DevOps
+PROFESSION="AWS/DevSecOps/Azure_DevOps"
 echo "class Name is $class, Batch Number $Batch, We are learing $PROFESSION"
